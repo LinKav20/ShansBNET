1da4c04437968018aed1f13e0ef15474a2cbf327
LinKa <maltseva-as@mail.ru>
2023-02-18 17:19:22 +0300

    navigation back action bar

--- app/src/main/java/com/github/linkav20/bnets/ui/product/ProductFragment.kt
+++ app/src/main/java/com/github/linkav20/bnets/ui/product/ProductFragment.kt
@@ -1,12 +1,16 @@
 package com.github.linkav20.bnets.ui.product
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.linkav20.bnets.databinding.FragmentProductBinding
+
 
 class ProductFragment : Fragment() {
 
@@ -24,7 +28,8 @@ class ProductFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        val actionBar = (activity as AppCompatActivity?)!!.supportActionBar
+        actionBar?.title = ""
     }
 
 }
