66e09551b1bbfae34bc0caa1eb7bc014bf7f0dad
LinKa <maltseva-as@mail.ru>
2023-02-18 18:05:53 +0300

    di step 1

---
+++ app/src/main/java/com/github/linkav20/bnets/App.kt
@@ -0,0 +1,16 @@
+package com.github.linkav20.bnets
+
+import android.app.Application
+import com.github.linkav20.core_network.di.DaggerNetworkComponent
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initDI()
+    }
+
+    private fun initDI() {
+        //DI.appComponent = DaggerAppComponent.builder().appContext(this).build()
+        DI.networkComponent = DaggerNetworkComponent.create()
+    }
+}
---
+++ app/src/main/java/com/github/linkav20/bnets/DI.kt
@@ -0,0 +1,11 @@
+package com.github.linkav20.bnets;
+
+import com.github.linkav20.core_network.di.NetworkComponent
+
+object DI {
+    //lateinit var appComponent: AppComponent
+     //   internal set
+
+    lateinit var networkComponent: NetworkComponent
+        internal set
+}
